c61c18c69361cca22f0cfd1dc1e1b3de3a862988
Wiktor Kulesza <wiktor.kulesza037@gmail.com>
2018-07-14 23:01:23 +0200

    added map_position function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
+#include <cstdlib>
 #include <iostream>
-
+#include <fstream>
+#include <windows.h>
+#include <conio.h>
 using namespace std;
 
 //TODO: Define a structure for representing a player
@@ -17,6 +20,7 @@ struct player {
 void map_create(int, int, char map[][100]);
 
 //TODO: Declare and define a function for handling user's moves and updating his position on the map
+void map_position(int &pos_x, int &pos_y, char map[][100], char map_help[]);
 
 //TODO: Declare and define a function for rendering a map
 void rendering(int lenght, int wide, char map[][100]);
@@ -26,16 +30,20 @@ void rendering(int lenght, int wide, char map[][100]);
 
 
 int main()
-{   player main_player;
+{  /* player main_player;
     cin>>main_player.name;
     cin>>main_player.column;
-    cin>>main_player.row;
+    cin>>main_player.row;*/
+    // int a aby petla ciagle chodzila xD to do usuniecia poxniej
+    int a=2;
 
-
+    int pos_x=1,pos_y=1;
     //TODO: Define the two dimensional char array that represents the map
 
     const int lenght=50, wide=100;
     char map[lenght][wide];
+    char map_help[4];
+    map_help[1]=' ';
 
     //TODO: Figure out a way to display signs in color
 
@@ -44,18 +52,33 @@ int main()
     //TODO: Create a simple loop that will ac as the game engine for now
     map_create(lenght, wide, map);
     rendering(lenght,wide,map);
+    while(a==2)
+    {
+        map_position(pos_x, pos_y, map, map_help);
+        system( "cls");
+        rendering(lenght,wide,map);
+    }
+
 
     return 0;
 }
 void map_create(int lenght, int wide,char map[][100])
 {
+     map[2][2]='o';
     for(int i=0; i<lenght; i++)
     {
+
         for(int i2=0; i2<wide-1; i2++)
         {
+
             if(i2==0)
             {
-                map[i][i2]='x';
+                map[i][i2]='=';
+
+            }
+             if(i2%2!=0 && (i==0 || i==lenght-1))
+            {
+                map[i][i2]='=';
                 continue;
             }
             if(i2%2!=0)
@@ -63,19 +86,125 @@ void map_create(int lenght, int wide,char map[][100])
                 map[i][i2]=' ';
                 continue;
             }
-            if((i==0 || i==lenght-1) || (i2==0 || i2==wide-2))
-               map[i][i2]='x';
+            if((i==0 || i==lenght-1))
+               map[i][i2]='=';
             else
                 map[i][i2]=' ';
+            if(i2==0 || i2==wide-2)
+                map[i][i2]='|';
+
 
         }
+
+
     }
+    //Some testing textures bellow
+    map[1][1]='X';
+    map[1][2]='a';
+    map[1][3]='b';
+
 }
 void rendering(int lenght, int wide, char map[][100])
 {
-    for(int i=0;i<lenght;i++)
+   for(int i=0,i2=0;i<lenght;i++,i2++)
     {
+
         cout.write(map[i],wide-1);
         cout<<'\n';
+
     }
 }
+
+void map_position(int &pos_x, int &pos_y, char map[][100], char map_help[])
+{
+    char movement;
+    movement = _getch();
+    switch(movement)
+        {
+        case 'd':
+            if(map[pos_y][pos_x+1]=='=' || map[pos_y][pos_x+1]=='|')
+            {
+                break;
+            }
+            map_help[2]=map[pos_y][pos_x];
+            if(map_help[1]==' ')
+            {
+                map[pos_y][pos_x]=' ';
+                map_help[1]='Z';
+            }
+            else
+            {
+                map[pos_y][pos_x]=map_help[3];
+            }
+            map_help[3]=map[pos_y][pos_x+1];
+            map[pos_y][pos_x+1]=map_help[2];
+            pos_x=pos_x+1;
+           break;
+        case 'a':
+            if(map[pos_y][pos_x-1]=='=' || map[pos_y][pos_x-1]=='|')
+            {
+                break;
+            }
+           map_help[2]=map[pos_y][pos_x];
+            if(map_help[1]==' ')
+            {
+            map[pos_y][pos_x]=' ';
+            map_help[1]='Z';
+            }
+            else
+            {
+                map[pos_y][pos_x]=map_help[3];
+            }
+            map_help[3]=map[pos_y][pos_x-1];
+            map[pos_y][pos_x-1]=map_help[2];
+
+
+           pos_x=pos_x-1;
+           break;
+        case 'w':
+            if(map[pos_y-1][pos_x]=='=' || map[pos_y-1][pos_x]=='|')
+            {
+                break;
+            }
+          map_help[2]=map[pos_y][pos_x];
+            if(map_help[1]==' ')
+            {
+            map[pos_y][pos_x]=' ';
+            map_help[1]='Z';
+            }
+            else
+            {
+                map[pos_y][pos_x]=map_help[3];
+            }
+            map_help[3]=map[pos_y-1][pos_x];
+            map[pos_y-1][pos_x]=map_help[2];
+
+
+           pos_y=pos_y-1;
+           break;
+        case 's':
+            if(map[pos_y+1][pos_x]=='=' || map[pos_y+1][pos_x]=='|')
+            {
+                break;
+            }
+           map_help[2]=map[pos_y][pos_x];
+            if(map_help[1]==' ')
+            {
+            map[pos_y][pos_x]=' ';
+            map_help[1]='Z';
+            }
+            else
+            {
+                map[pos_y][pos_x]=map_help[3];
+            }
+            map_help[3]=map[pos_y+1][pos_x];
+            map[pos_y+1][pos_x]=map_help[2];
+
+
+           pos_y=pos_y+1;
+           break;
+        default:
+            break;
+        }
+}
+
